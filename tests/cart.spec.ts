3be5fc8f044a1a90ac23c53815c3bdc45503f192
kejters <kasia.kap@gmail.com>
2024-07-06 13:17:08 +0200

    Refactoring of tests

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,17 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('cart operations', () => {
-  test('add product to cart', async ({ page }) => {
-    //Arrange
-    const url = 'https://www.saucedemo.com/';
+
+  test.beforeEach(async ({ page }) => {
     const correct_ID = 'standard_user';
     const correct_password = 'secret_sauce';
 
-    //Act
-    await page.goto(url);
+    await page.goto('/');
     await page.locator('#user-name').fill(correct_ID);
     await page.locator('#password').fill(correct_password);
     await page.locator('#login-button').click();
+  });
+  
+  test('add product to cart', async ({ page }) => {
+    //Arrange
+
+    //Act
     await page.locator('#add-to-cart-sauce-labs-backpack').click();
     await page.locator('#shopping_cart_container').click();
 
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to shop', () => {
+  
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
   test('login with correct credentials', async ({ page }) => {
     //Arrange
-    const url = 'https://www.saucedemo.com/';
     const correct_ID = 'standard_user';
     const correct_password = 'secret_sauce';
 
     //Act
-    await page.goto(url);
     await page.locator('#user-name').fill(correct_ID);
     await page.locator('#password').fill(correct_password);
     await page.locator('#login-button').click();
@@ -19,12 +22,10 @@ test.describe('User login to shop', () => {
 
   test('login with incorrect password', async ({ page }) => {
     //Arrange
-    const url = 'https://www.saucedemo.com/';
     const correct_ID = 'standard_user';
     const incorrect_password = '1234';
 
     //Act
-    await page.goto(url);
     await page.locator('#user-name').fill(correct_ID);
     await page.locator('#password').fill(incorrect_password);
     await page.locator('#login-button').click();
@@ -36,12 +37,10 @@ test.describe('User login to shop', () => {
   test('login with incorrect user', async ({ page }) => {
 
     //Arrange
-    const url = 'https://www.saucedemo.com/';
     const incorrect_ID = 'standard_user222';
     const correct_password = 'secret_sauce';
 
     //Act
-    await page.goto(url);
     await page.locator('#user-name').fill(incorrect_ID);
     await page.locator('#password').fill(correct_password);
     await page.locator('#login-button').click();
@@ -52,11 +51,9 @@ test.describe('User login to shop', () => {
 
   test('login with empty user', async ({ page }) => {
     //Arrange
-    const url = 'https://www.saucedemo.com/';
     const correct_password = 'secret_sauce'; 
     
     //Act
-    await page.goto(url);
     await page.locator('#password').fill(correct_password);
     await page.locator('#login-button').click();
 
@@ -67,11 +64,9 @@ test.describe('User login to shop', () => {
   test('login with empty password', async ({ page }) => {
 
     //Arrange
-    const url = 'https://www.saucedemo.com/';
     const correct_ID = 'standard_user';
 
     //Act
-    await page.goto(url);
     await page.locator('#user-name').fill(correct_ID);
     await page.locator('#login-button').click();
 
